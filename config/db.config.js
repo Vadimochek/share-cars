90eed94396a9a70a22df126f35119814be2ee616
Vadimochek <56549726+Vadimochek@users.noreply.github.com>
2021-12-01 15:10:47 +0300

    Update db.config.js

--- config/db.config.js
+++ config/db.config.js
@@ -2,7 +2,7 @@ module.exports = {
     HOST: "ec2-34-250-16-127.eu-west-1.compute.amazonaws.com",
     USER: "crgekcbneljqma",
     PASSWORD: "d5cc5a7d388664a0fe52ecfe0a868d2dcad68fafa4820f24b008aeda84eb1b49",
-    DB: "postgresql-infinite-95753",
+    DB: "d2a2qgtatqt8nk",
     dialect: "postgres",
     pool: {
       max: 5,
